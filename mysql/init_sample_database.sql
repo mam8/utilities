5092a1bbf8ab6cc3f8c597aaf086f4946d959d12
mam8 <martin.t.ma@emc.com>
2017-06-07 15:59:11 +0800

    Update init_sample_database.sql

--- mysql/init_sample_database.sql
+++ mysql/init_sample_database.sql
@@ -1,1 +1,23 @@
 create database my_student;
+
+CREATE TABLE Student (
+    ID int NOT NULL AUTO_INCREMENT,
+    Name varchar(255) NOT NULL,
+    Age int,
+    Grade int,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Lesson (
+    ID int NOT NULL AUTO_INCREMENT,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Score (
+    Score int NOT NULL,
+    StudentID int,
+    LessonID int,
+    FOREIGN KEY (StudentID) REFERENCES Student(ID)
+    FOREIGN KEY (LessonID) REFERENCES Lesson(ID)
+);
